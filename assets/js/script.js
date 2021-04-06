21de59d43c79b037bce70c15091a86c0b7bf11e9
Reesh Miranda <rtmiranda18@gmail.com>
2021-04-06 12:42:25 +0800

    input date via moment.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,16 +14,30 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&units=i
   .then(response => response.json())
   .then(function (data) {
      displayBox.textContent=data.name + data.main.temp + data.wind.speed + data.main.humidity;
-  });
+     var cityName = document.querySelector(".cityName");
+     var cityTime = document.querySelector(".cityTime");
+     var time = moment().format("dddd, MMMM Do YYYY");
+     cityName.innerHTML = input.value;
+     cityTime.innerHTML = time;
+    });
 });
+
 
 //For the UV Index
 //Call the API app using fetch 
 //Make a variable that holds any city name that is searched
 //Addeventlistener to listen to the click of the button to capture info from the text area
 //that info is put into a query 
-fetch('http://api.openweathermap.org/data/2.5/uvi?lat={lat}&lon={lon}&appid=82a1cdbfc5805679db4bf205d606147c')
-  .then(response => response.json())
+
+// search.addEventListener("submit", function(event) {
+// event.preventDefault();
+// fetch('http://api.openweathermap.org/data/2.5/uvi?lat={lat}&lon={lon}&appid=82a1cdbfc5805679db4bf205d606147c')
+//   .then(response => response.json())
+//   .then(function (data) {
+//     //displayBox.;
+//     console.log(data);
+//  });
+// });
 
 //For the 5 day forecast
 //Call the API app using fetch 
